4f3eff3f6b725dbaf95f5a28016c1ee33043ddd0
Herve2iko <ihervedelice@gmail.com>
2021-02-18 17:29:36 +0200

    admin

--- ijamboAdmin/urls.py
+++ ijamboAdmin/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .import views
+from ijamboAdmin import views
 urlpatterns = [
     path('', views.home, name='homeAdmin'),
     path('addMusic/', views.MusicRegester, name='music'),
+    path('ddMusic/', views.MusicRegester, name='mus'),
     path('addAlbum/', views.AlbumRegester, name='album'),
-    path('addEvent/', views.EventRegester, name='event'),
+    path('addEvent/', views.EventRegester, name='addevent'),
     path('listMusic/', views.MusicListe, name='lsMusic'),
     path('UpdateMusic/<int:ms_id>', views.UpdateMusic, name='upMusic'),
 
