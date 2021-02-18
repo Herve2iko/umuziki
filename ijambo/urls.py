de15f66ab0114e5c8ed6da100b1ab0332a5f736a
Herve2iko <ihervedelice@gmail.com>
2021-02-18 17:27:28 +0200

    contact url

--- ijambo/urls.py
+++ ijambo/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.HomeView.as_view(), name='home'),
+    path('contare/', views.contactrecever, name ='conta'),
     path('register/', views.register, name='register'),
     path('login/', views.connexion, name='login'),
     path('logout/', views.deconexion, name='logout'),
